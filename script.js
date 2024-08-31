b1d0518a45b5fdb9c4df56a1e827a7f4b51bd105
rutan <t995771take@gmail.com>
2024-09-01 01:55:03 +0900

    refact css

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-document.querySelectorAll("#menu .btn").forEach((btn) => {
-  btn.addEventListener("click", function (event) {
-    event.preventDefault(); // デフォルトのリンク動作を防止
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("#menu .btn").forEach((btn) => {
+    btn.addEventListener("click", function (event) {
+      console.log("log");
+      event.preventDefault(); // デフォルトのリンク動作を防止
+      const contentUrl = this.getAttribute("data-content"); // 対応するコンテンツファイルのURLを取得
+      const contentDiv = document.getElementById("content");
 
-    const contentUrl = this.getAttribute("data-content"); // 対応するコンテンツファイルのURLを取得
-    const contentDiv = document.getElementById("content");
-
-    // コンテンツを非同期で読み込む
-    fetch(contentUrl)
-      .then((response) => response.text())
-      .then((data) => {
-        contentDiv.innerHTML = data; // 取得したコンテンツを挿入
-      })
-      .catch((error) => {
-        console.error("Error loading content:", error);
-        contentDiv.innerHTML = "<p>コンテンツを読み込めませんでした。</p>";
-      });
+      // コンテンツを非同期で読み込む
+      fetch(contentUrl)
+        .then((response) => response.text())
+        .then((data) => {
+          contentDiv.innerHTML = data; // 取得したコンテンツを挿入
+        })
+        .catch((error) => {
+          console.error("Error loading content:", error);
+          contentDiv.innerHTML = "<p>コンテンツを読み込めませんでした。</p>";
+        });
+    });
   });
 });
